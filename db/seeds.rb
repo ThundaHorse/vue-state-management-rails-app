d60ed37afcaa348edf3c208b51ad81ea54869447
ThundaHorse <abeeeeekim@gmail.com>
2019-08-13 18:53:15 -0400

    adding data for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# todos = [{title: "Vacuum"}, {title: "Study"}, {title: "Organize desk"}, {title: "Beat it"}]
-# todos.each do |todo| 
-#   Todo.create(todo)
-# end 
+todos = [{title: "Vacuum"}, {title: "Study"}, {title: "Organize desk"}, {title: "Clean room"}, {title: "Clean kitchen"}]
+todos.each do |todo| 
+  Todo.create(todo)
+end 
